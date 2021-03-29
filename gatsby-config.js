04154229d0eeed5c0256b3b0415dd17ef52b82a5
achoosv <44436003+achoosv@users.noreply.github.com>
2021-03-29 17:55:55 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
   siteMetadata: {
+    siteTitle: `Vasanth`,
     siteTitleAlt: `Minimal Blog - Gatsby Theme`,
   },
   plugins: [

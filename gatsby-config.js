a0c0af078090be421f030b0b1221459dde7e642c
achoosv <44436003+achoosv@users.noreply.github.com>
2021-03-29 17:14:54 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,12 +35,7 @@ module.exports = {
         ],
       },
     },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
-      },
-    },
+    
     `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-manifest`,
